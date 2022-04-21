dc28886ac1ac0dbd4497d6c76c29f096a3dc0bbd
KateRiinaO <orrava.kate@gmail.com>
2022-04-21 22:06:55 +0300

    Added footer (GitHub link)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <p>
+          This project was coded by Katerina Orrava and is open-sourced on{" "}
+          <a
+            href="https://github.com/KateRiinaO/react-app-weather/commit/f7d12d8cc3a2bb57ff767584468e2a72c3c640dc"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://admirable-bunny-59f2ff.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
